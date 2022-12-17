a8adb71ad13a6184ccb3bc039ab6ac57d13c2d33
Leonard Paturel <leonardpaturel@gmail.com>
2022-12-17 17:55:03 +0000

    created another dump file for testing

---
+++ data/dump.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import sqlite3
+import os
+
+
+
+cwd = os.getcwd()
+db_path = os.path.join(cwd, "database/clean_data.db")
+
+db_connection = sqlite3.connect(db_path)
+cursor = db_connection.cursor()
+
+# read the data from the database table into a DataFrame
+df = pd.read_sql("SELECT * FROM treasury", db_connection)
+
+# export the DataFrame to an Excel file
+df.to_excel("test.xlsx")
+
+# read the data from the database table into a DataFrame
+df = pd.read_sql("SELECT * FROM treasury", db_connection)
+
+# export the DataFrame to an Excel file
+df.to_excel("test.xlsx")
